1283f3fe497f5dc2cae13dad054440019f519e48
Chris Rocco <chris.rocco7@gmail.com>
2018-06-20 14:18:22 -0500

    hid twilio phone

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const client = require('twilio')(process.env.TWILIO_ACC_ID, process.env.TWILIO_API_KEY)
+
+const twilioPhone = process.env.TWILIO_PHONE
 const victim = process.env.VICTIM_PHONE
 
 client.calls
 	.create({
 	  url: 'https://handler.twilio.com/twiml/EHbc5b1363b061454382ada2f3816cc8c2',
 	  to: victim,
-	  from: '+12056390077'
+	  from: twilioPhone
 	})
 	.then(call => console.log(call.sid))
 	.done()
